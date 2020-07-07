365eabdf0906e4387387f24a0726e3741a4b5914
Vitaly Gavrilov <vgavrilov@gotrg.com>
2020-07-07 13:14:16 +0300

    fix secret

--- Parser2/LenovoApi/LenovoCatalogClient.cs
+++ Parser2/LenovoApi/LenovoCatalogClient.cs
@@ -31,7 +31,7 @@ namespace Parser2.LenovoApi
                        "],\"languageCodeList\": [\"E\"],\"plantList\": [\"" + plant + "\"]} ";
             var content = new StringContent(data);
             content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
-            var token = await LenovoTokenClient.GetTokenFromCache("40tI7PHRRHUoToHV0eSG7rulr38a", "9N8MHYWmNq6bH1RTZjoVJjCmaKUa");
+            var token = await LenovoTokenClient.GetTokenFromCache("40tI7PHRRHUoToHV0eSG7rulr38a", "iJMoZIjJyd2ZERXFwQw8oRhuNH8a");
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
             CatalogResponse obj = null;
             while (obj == null || (obj.code == "-1" && obj.msg.Contains("Null")))
